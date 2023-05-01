6a51189efecff91e9c1b6e91a0c39e73df3039f4
zelda <126986145+zeldaskywalker@users.noreply.github.com>
2023-05-01 13:31:03 -0400

    Update static url and root in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,13 +126,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_ROOT ='mts-web-app/mts/static'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
